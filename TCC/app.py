02393a6dabc417d3cb78302d8f1fe5d6171782af
RIQUELME HENRIQUE MOTA <riquelmemota2016@gmail.com>
2024-07-17 06:58:52 -0300

    Update app.py

--- TCC/app.py
+++ TCC/app.py
@@ -5,3 +5,21 @@ from flask_cors import CORS
 app = Flask(__name__)
 CORS(app)
 
+# Conexão com o MongoDB
+client = MongoClient('mongodb+srv://teste:1234@cluster0.gn1h0by.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
+db = client['usuarios_db']
+collection = db['usuarios']
+
+@app.route('/cadastrar', methods=['POST'])
+def cadastrar():
+    data = request.json
+    print(f"Recebido: {data}")  # Log de recebimento de dados
+    pergunta = data['pergunta']
+    resposta = int(data['resposta'])  # Converter idade para inteiro, se necessário
+
+    # Inserir no MongoDB
+    collection.insert_one({'pergunta': pergunta, 'resposta': resposta})
+    return jsonify({"message": "Usuário cadastrado com sucesso!"}), 201
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0')
