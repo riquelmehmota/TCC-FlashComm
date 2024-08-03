6ff157f63ebd595f873638c518e3e3ef196321cb
Euzebio <PedroGitDev@gmail.com>
2024-08-03 12:43:35 -0300

    n

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -9,6 +9,31 @@ passport.use(new LocalStrategy (function verify(username, password,cd) {
         if (!user) {
             return cd(null, false);
         }
-        
+        crypto.pbkdf2(password, user.salt, 310000, 32, 'sha256', function(err, key) {
+            if (err) { return cb(err); }
+            if (!crypto.timingSafeEqual(row.hashed_password, hashedPassword)) {
+                return cb(null, false, { message: 'Incorrect username or password.' });
+            }
+            return cb(null, row);
+        });
     });
 }));
+
+passport.serializeUser(function(user, cb) {
+    process.nextTick(function() {
+      cb(null, { id: user.id, username: user.username });
+    });
+});
+  
+passport.deserializeUser(function(user, cb) {
+    process.nextTick(function() {
+        return cb(null, user);
+    });
+});
+
+module.exports = passport;
+
+
+
+
+
--- server/server.js
+++ server/server.js
@@ -1,17 +1,38 @@
 const express = require('express');
+const app = express();
 const express_json = require('express-json');
 const db = require('./models/db');
 const Users = require('./routes/Users');
 const Decks = require('./routes/Decks');
 const Cards = require('./routes/Cards');
 const cors = require('cors');
+var logger = require('morgan');
+var passport = require('passport');
+var session = require('express-session');
+
+const session = require('express-session');
+
+const MySQLStore = require('express-mysql-session')(session);
 
 
 db.sequelize.sync({ force: true }).then(() => {
     console.log('Drop and re-sync db.');
 });
 
-const app = express();
+app.use(session({
+    secret: 'keyboard',
+    resave: false,
+    saveUninitialized: false,
+    store: new MySQLStore({
+        db: 'tcc',
+        host: 'localhost',
+        port: 3306,
+    })
+}));
+
+app.use(passport.authenticate('session'));
+
+
 app.use(express_json());
 app.use(cors());
 
